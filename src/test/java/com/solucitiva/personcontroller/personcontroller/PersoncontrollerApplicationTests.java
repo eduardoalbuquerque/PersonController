df1de767dbc8cfb187a838b1c13e2e917689cc09
Eduardo Albuquerque <eduardo.albuquerque@hotmail.com.br>
2020-12-14 11:38:10 -0300

    Start project - first commit

---
+++ src/test/java/com/solucitiva/personcontroller/personcontroller/PersoncontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solucitiva.personcontroller.personcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersoncontrollerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
